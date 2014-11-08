31df6d088add385944824c1cba160f52edd42a1b
Charles Horton <charlie@horton5.com>
2014-11-07 21:36:20 -0500

    made my first ogre program and it works! YAY

---
+++ OgreHelloWorld/Build/CMakeFiles/OgreHelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/OgreHelloWorld.dir/BaseApplication.cpp.o"
+  "CMakeFiles/OgreHelloWorld.dir/TutorialApplication.cpp.o"
+  "dist/bin/OgreHelloWorld.pdb"
+  "dist/bin/OgreHelloWorld"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/OgreHelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
