7e7b8e18a6dc788046bb277d1fed9d8216bd051e
Charles <charlie@horton5.com>
2014-11-10 17:25:29 -0500

    update

--- Ogre/BasicTutorial3/Build/CMakeFiles/Makefile.cmake
+++ Ogre/BasicTutorial3/Build/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ SET(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 SET(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/OgreApp.dir/DependInfo.cmake"
+  "CMakeFiles/BasicTutorial3.dir/DependInfo.cmake"
   )
